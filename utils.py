09eb341651cd3c9f547a28a87c44110df2611ff6
Walcy Rios <wjuniorrios@gmail.com>
2022-04-20 09:34:23 -0300

    comentarios e adicionar separator

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import json
 
-def conll2pandas(path: str):
+
+def conll2pandas(path: str, sep=' '):
     """Convert conll file to pandas dataframe
 
     Args:
@@ -10,24 +11,23 @@ def conll2pandas(path: str):
     Returns:
         pandas.DataFrame: pandas DataFrame with text and tags cols
     """
-    f = open(path, 'r', encoding='UTF8')
+    with open(path, 'r', encoding='UTF8') as f:
+        texts = []
+        labels = []
 
-    texts = []
-    labels = []
+        words = []
+        tags = []
+        for line in f.readlines():
+            line_list = line.split(sep)
+            if line_list[0] != '\n':
+                words.append(line_list[0])
+                tags.append(line_list[-1][:-1])
+            else:
+                texts.append(words.copy())
+                labels.append(tags.copy())
+                words.clear()
+                tags.clear()
 
-    words = []
-    tags = []
-    for line in f.readlines():  
-        line_list = line.split(' ')
-        if line_list[0] != '\n':
-            words.append(line_list[0])
-            tags.append(line_list[-1][:-1]) 
-        else:
-            texts.append(words.copy())
-            labels.append(tags.copy())
-            words.clear()
-            tags.clear() 
-        
     df = pd.DataFrame()
     df['text'] = texts
     df['tags'] = labels
@@ -43,17 +43,24 @@ def pandas2conll(df, fname):
         fname (str): filename to save eg. dataset.conll
     """
     rows = []
-        
+
     for text, ent in zip(df['text'], df['tags']):
-        for word, tag in zip(text, ent):            
+        for word, tag in zip(text, ent):
             rows.append(str(word)+' O O '+str(tag)+'\n')
         rows.append('\n')
-        
-    
+
     with open(fname, 'w', encoding="utf-8") as f:
         f.writelines(rows)
 
-def pandas2json(df, fname: str):    
+
+def pandas2json(df, fname: str):
+    """Convert pandas to json file
+
+    Args:
+        df (pd.DataFrame): Dataframe Object
+        fname (str): file name
+    """
+
     texts = []
     for i in range(len(df)):
         text_dict = {
@@ -61,8 +68,7 @@ def pandas2json(df, fname: str):
             "tags": df['tags'].iloc[i]
         }
         texts.append(text_dict)
-        
+
     with open(fname, 'w', encoding='utf8') as file:
         for text in texts:
             json.dump(text, file, ensure_ascii=False)
-
