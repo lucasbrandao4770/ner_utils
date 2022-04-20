63bdb6903839f1cc762d008325c1981bd7265093
Walcy Rios <wjuniorrios@gmail.com>
2022-04-20 09:34:43 -0300

    docstring

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -3,6 +3,15 @@ import numpy as np
 from typing import List
 
 def filter_length_dataset(df, length_to_filter = 256):
+    """Low pass filter with length of sentences
+
+    Args:
+        df (pd.DataFrame): The dataframe object
+        length_to_filter (int, optional): The sentence max length accepted. Defaults to 256.
+
+    Returns:
+        pd.DataFrame: The dataframe object filtered
+    """
     assert length_to_filter > 0, 'Length must be positive'
     df['quantidadeTokens'] = df['text'].apply(len)   # QUANTIDADE DE SENTENÇAS/MAX/MEDIA E MIN
     dataset_filtered = df[df['quantidadeTokens'] <= length_to_filter]  
@@ -11,6 +20,22 @@ def filter_length_dataset(df, length_to_filter = 256):
     dataset_filtered = dataset_filtered.reset_index(drop=True)
     return dataset_filtered
 
+def remove_entites(df, entities_to_remove: List):
+    """Function to remove a list of entities (tags) in dataFrame
+    All tags to be removed are replaced to 'O'
+
+    Args:
+        df (pd.DataFrame): The dataframe object
+        entities_to_remove (List): List of entities (tags) to be removed
+
+    Returns:
+        pd.Dataframe: The dataframe object without the list of tags
+    """
+    print('Entidades removidas', entities_to_remove)
+
+    df['tags'] = df['tags'].apply(lambda tags: ['O' if tag[2:] in entities_to_remove else tag for tag in tags ] )
+    
+    return df
 
 def filter_entities(df, minimum_entity_ratio: 0):
     assert minimum_entity_ratio > 0 and minimum_entity_ratio < 1, 'Ratio must be between 0 and 1'
@@ -35,16 +60,19 @@ def filter_entities(df, minimum_entity_ratio: 0):
 
     return remove_entites(df, entities_to_remove=entities_to_remove)
 
-def remove_entites(df, entities_to_remove: List):
-    print('Entidades removidas', entities_to_remove)
-
-    df['tags'] = df['tags'].apply(lambda tags: ['O' if tag[2:] in entities_to_remove else tag for tag in tags ] )
-    
-    return df
-
 
 
 def undersampling_null_sentences(df, ratio_to_remove=0.5):
+    """Apply undersampling in sentences with full tags 'O'
+
+    Args:
+        df (pd.Dataframe): dataframe object
+        ratio_to_remove (float, optional): undersampling Ratio. Defaults to 0.5.
+
+    Returns:
+        pd.dataFrame: DataFrame with undersampling
+    """
+
     # sentences with ALL TAGS '0'
     df['nullSentences'] = df['tags'].apply(lambda tags: all([tag == 'O' for tag in tags] ))
 
@@ -56,6 +84,17 @@ def undersampling_null_sentences(df, ratio_to_remove=0.5):
     return dataset_filtered.reset_index()
 
 def undersampling_entity(df, undersampling_tags, ratio_to_remove=0.5):
+    """Apply undersampling with specific tags
+
+    Args:
+        df (pd.dataFrame): Dataframe object
+        undersampling_tags (List[String]): A List of Tags to apply undersampling
+        ratio_to_remove (float, optional): Undersampling Ratio. Defaults to 0.5.
+
+    Returns:
+        pd.dataFrame: Dataframe object with tags undersampled
+    """
+
     # sentences with at least one TAG
     df['withEntity'] = df['tags'].apply(lambda tags: any([tag[2:] in undersampling_tags for tag in tags ] ))
 
